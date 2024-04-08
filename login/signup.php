20dc060ebcef855a9bce8fda983111878f98924a
kofinco <contactsedemkofi@gmail.com>
2024-04-08 21:35:58 +0000

    front end validation

--- login/signup.php
+++ login/signup.php
@@ -82,12 +82,17 @@
                 var email = $("#email").val();
                 var password = $("#password").val();
                 var confirmPassword = $("#confirm-password").val();
+                var domainPattern = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                var domain = email.substring(email.lastIndexOf("@") +1);
                 if (email === "" || password === "" || confirmPassword === "") {
                     event.preventDefault();
                     alert("Please fill in all fields.");
                 } else if (password !== confirmPassword) {
                     event.preventDefault();
                     alert("Passwords do not match.");
+                } else if (!domainPattern.test(domain)) { // Test if domain matches the regular expression
+                    event.preventDefault();
+                    alert("Invalid email address.");
                 }
             });
         });
