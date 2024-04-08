ee9f48638600580b74f51cc0a3a7a18a04a33561
sedem <sedem.amediku@ashesi.edu.gh>
2024-04-08 17:42:36 +0000

    extra css

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
     <script src="js/index.js"></script>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
