18f177f1c25a8b990dc7a2960ece08e7ccc8b0c8
sedem <sedem.amediku@ashesi.edu.gh>
2024-04-08 16:36:11 +0000

    signin.php -> login.php

--- login/signout.php
+++ login/signout.php
@@ -6,7 +6,7 @@
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
 
-    header('Location: signin.php');
+    header('Location: login.php');
     exit();
 ?>
 
