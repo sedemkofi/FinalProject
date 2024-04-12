6f516658ccaab62513edbe6ba8c51b66743fd980
sedemkofi <sedem.amediku@ashesi.edu.gh>
2024-04-12 11:14:15 +0000

    connection

--- settings/connection.php
+++ settings/connection.php
@@ -4,14 +4,10 @@ try {
     $SERVER="127.0.0.1";
     $USERNAME = "root"; 
     $PASSWORD= "E!x8Uc16t!mf";
-    $DB_NAME ="musiccollab";
+    $DB_NAME ="MusicCollab";
     
     $conn = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DB_NAME);
 } catch (\Throwable $th) {
     header("Location: ../index.php?error=connection-error");
 }
-    
-    
-
-
 ?>
