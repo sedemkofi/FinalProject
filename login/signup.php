3a7db20e518545402b9cfc8ddbad96e479cc705a
sedemkofi <sedem.amediku@ashesi.edu.gh>
2024-04-12 09:30:37 +0000

    fixing sign up

--- login/signup.php
+++ login/signup.php
@@ -3,11 +3,14 @@
     include '../settings/core.php';
     checkLogin();
 ?>
-<!DOCTYPE html>
-<html>
 
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <title>Register</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wavers - SignUp</title>
+        
         <link rel="icon" href="../images/waveform.svg">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
